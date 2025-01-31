0e0506e1675ef230fe4bf3314a2e47b475aea857
ArnaldoLima12 <arnaldolima588@gmail.com>
2025-01-31 00:18:44 -0300

    fix ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,10 +7,8 @@ export default function WelcomeScreen() {
     <View className="flex-1 justify-center items-center bg-blue-50">
       <Text className="text-2xl font-bold text-blue-900">Bem-vindo!</Text>
 
-      <Link href="/options">
-        <TouchableOpacity className="mt-6 bg-blue-500 px-6 py-2 rounded-lg">
-          <Text className="text-white font-semibold">Ir para Opções</Text>
-        </TouchableOpacity>
+      <Link href="/options" className="mt-3">
+        <Text className="text-blue-500 font-semibold">Ir para Opções</Text>
       </Link>
     </View>
   );
